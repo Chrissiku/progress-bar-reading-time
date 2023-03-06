2ea4f74efd5430a9a201b23c5d9c3d7c0f2a38d3
Chris Siku <chrissiku5@gmail.com>
2023-03-06 22:11:55 +0200

    Calculate the progressbar value

--- app/javascript/controllers/progress_controller.js
+++ app/javascript/controllers/progress_controller.js
@@ -1,7 +1,14 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus";
 
 // Connects to data-controller="progress"
 export default class extends Controller {
   connect() {
+    window.addEventListener("scroll", this.calculateProgress.bind(this));
+  }
+
+  calculateProgress() {
+    const totalHeight = document.body.scrollHeight - window.innerHeight;
+    const progressPercent = (window.pageYOffset / totalHeight) * 100;
+    this.element.style.width = `${progressPercent}%`;
   }
 }
