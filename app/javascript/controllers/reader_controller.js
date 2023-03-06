8ddd52f7c2cd0b3f7edce14c8b084163fc8d45ae
Chris Siku <chrissiku5@gmail.com>
2023-03-06 21:52:12 +0200

    calculate and display the reading time from stimulus controller

--- app/javascript/controllers/reader_controller.js
+++ app/javascript/controllers/reader_controller.js
@@ -1,7 +1,18 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus";
 
 // Connects to data-controller="reader"
 export default class extends Controller {
+  static targets = ["readingTime", "body"];
   connect() {
+    this.readingTime();
+  }
+
+  readingTime() {
+    const text = this.bodyTarget.textContent;
+    const wordsPerMinutes = 256;
+    const numberOfWords = text.split(/\s/g).length;
+    const minutes = numberOfWords / wordsPerMinutes;
+    const readTime = Math.ceil(minutes);
+    this.readingTimeTarget.textContent = `${readTime} min read`;
   }
 }
